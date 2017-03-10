825b39c9a84b207209b0d64cd9582cd3466a6c0a
unknown <csdevelopment1@CSDevelopment1.cdccsin.local>
2017-03-10 12:06:30 +0530

    restructure the directory

--- js/reactLibrary.php
+++ js/reactLibrary.php
@@ -1,5 +1,5 @@
 <!--<script src="/js/react/<?php echo SITE_ID; ?>/react-0.13.3.js"></script>-->
-<script src="/js/react/<?php echo SITE_ID; ?>/react-15.0.0.js"></script>
-<script src="/js/react/<?php echo SITE_ID; ?>/react-dom-15.0.0.js"></script>
-<script src="/js/react/<?php echo SITE_ID; ?>/JSXTransformer-0.13.3.js"></script>
-<script src="/js/react/<?php echo SITE_ID; ?>/jsx/common-lib.jsx" type="text/jsx"></script>
+<script src="/js/react/3/react-15.0.0.js"></script>
+<script src="/js/react/3/react-dom-15.0.0.js"></script>
+<script src="/js/react/3/JSXTransformer-0.13.3.js"></script>
+<script src="/js/react/3/jsx/common-lib.jsx" type="text/jsx"></script>
